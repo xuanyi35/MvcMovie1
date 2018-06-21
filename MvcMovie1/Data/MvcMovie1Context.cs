53e256ebdd538ed64bc7af341551c8a5d4bd7561
xuanyi35 <31713273+xuanyi35@users.noreply.github.com>
2018-06-20 21:39:43 -0600

    Initial commit

---
+++ MvcMovie1/Data/MvcMovie1Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie1.Models
+{
+    public class MvcMovie1Context : DbContext
+    {
+        public MvcMovie1Context (DbContextOptions<MvcMovie1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie1.Models.Movie> Movie { get; set; }
+    }
+}
