9370bc2b82cdd78b0135e4b4399a259deabaa086
xuanyi35 <31713273+xuanyi35@users.noreply.github.com>
2018-06-26 22:09:44 -0600

    adjust layout

--- MvcMovie1/Models/Movie.cs
+++ MvcMovie1/Models/Movie.cs
@@ -10,13 +10,18 @@ namespace MvcMovie1.Models
     public class Movie
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; }
-
+        [Required]
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+        [Required]
         public string Genre { get; set; }
+        [Required]
         public decimal Price { get; set; }
     }
+
+    
 }
